f3321d4c44641f8f800aa67d3e40077d970f4b8e
Ashish Bhart <1thunder317@gmail.com>
2026-09-07 21:40:38 +0530

    T2.09 Quadrant Finder

---
+++ 02-conditionals/T2_09_Quadrant_Finder.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+
+    scanf("%d %d", &x, &y);
+
+    if (x == 0 && y == 0) {
+        printf("Origin");
+    }
+    else if (y == 0) {
+        printf("X-axis");
+    }
+    else if (x == 0) {
+        printf("Y-axis");
+    }
+    else if (x > 0 && y > 0) {
+        printf("Quadrant I");
+    }
+    else if (x < 0 && y > 0) {
+        printf("Quadrant II");
+    }
+    else if (x < 0 && y < 0) {
+        printf("Quadrant III");
+    }
+    else {
+        printf("Quadrant IV");
+    }
+
+    return 0;
+}
