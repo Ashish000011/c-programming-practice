6b248ed422a2a57be875043864319fed7ef49b1f
Ashish Bhart <1thunder317@gmail.com>
2026-09-13 23:49:00 +0530

    T3_08_prime_in_range

---
+++ 03-loops/T3_08_Prime_in_range.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, prime;
+
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i++) {
+        prime = 1;
+
+        if (i < 2)
+            prime = 0;
+        else {
+            for (j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    prime = 0;
+                    break;
+                }
+            }
+        }
+
+        if (prime)
+            printf("%d ", i);
+    }
+
+    printf("\n");
+
+    return 0;
+}
