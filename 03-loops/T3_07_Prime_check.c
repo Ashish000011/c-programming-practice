4d24c8350aba156526853e515113ef96a0ba32d4
Ashish Bhart <1thunder317@gmail.com>
2026-09-13 23:46:46 +0530

    T3_07_prime_check

---
+++ 03-loops/T3_07_Prime_check.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, prime = 1;
+
+    scanf("%d", &n);
+
+    if (n < 2) {
+        prime = 0;
+    } else {
+        for (i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                prime = 0;
+                break;
+            }
+        }
+    }
+
+    if (prime)
+        printf("Prime\n");
+    else
+        printf("Not prime\n");
+
+    return 0;
+}
